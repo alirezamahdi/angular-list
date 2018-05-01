355b0d9d6f59cdb0c8dd08411fc34c710a747ac6
alirezamahdi <alirezamahdi87@yahoo.com>
2018-05-01 14:47:55 +0430

    this is app writing code with angular 5.2 version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('angular2', 'http://angular.io', 3),
+      new Article('hassan', 'http://hassan1397@yahoo.com', 2),
+      new Article('hossein', 'http://hossein.mashmadgholi@yahoo.com', 1)
+    ];
+  }
+  addArticle(title: HTMLInputElement, link:HTMLInputElement): boolean {
+    console.log(`adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
